82fa77fd43d90a785a0db713267b58ce4a190ac3
Averbea <42838519+Averbea@users.noreply.github.com>
2023-05-02 20:00:38 +0200

    fix infinite fetching bug

--- client/src/hooks/useFetchAndUpdateArrOnScroll.ts
+++ client/src/hooks/useFetchAndUpdateArrOnScroll.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import useIsInViewport from "./useIsInViewport"
 import useDebounce from "./useDebounce"
 
@@ -6,13 +6,20 @@ export function useFetchAndUpdateArrOnScroll<T>( arr: T[], toCall: Function){
     const lastRef = useRef<HTMLDivElement>(null)
     const endInViewport = useIsInViewport(lastRef)
     const skipForFetch = useDebounce(arr.length)
+    const [alreadyCalled, setAlreadyCalled] = useState(false)
   
     useEffect(() => {
-      if(!endInViewport) return
+      if(!endInViewport || alreadyCalled) return
       if(arr.length !== skipForFetch) return
+      setAlreadyCalled(true)
        toCall()
-    }, [skipForFetch, endInViewport, arr.length, toCall])
-  
+    }, [skipForFetch, endInViewport, arr.length, toCall, alreadyCalled])
+    
+    useEffect(() => {
+      if (!endInViewport) {
+        setAlreadyCalled(false)
+      }
+    }, [endInViewport])
   
     return {
       lastRef
